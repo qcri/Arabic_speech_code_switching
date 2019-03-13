0e0873b375c7ec040ea141bfff0989b419034a3d
Ahmed Ali <amali@qf.org.qa>
2019-03-13 05:50:44 -0400

    add phoneme and grapheme Arabic features

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
-for phoneme in HU RU CZ; do
-    python combineCTM.py --wordindex data/MGB3_EGY/txt/words_QATS.ctm data/MGB3_EGY/txt/phoneme_${phoneme}.ctm words_${phoneme}.ctm
-    python makeFeat.py --wordindex words_${phoneme}.ctm data/MGB3_EGY/txt/{E,K,S}.txt.bw data/MGB3_EGY/txt/words_${phoneme}_data.feat
+for x in phoneme_HU phoneme_RU phoneme_CZ phoneme_AR grapheme_AR; do
+    python combineCTM.py --wordindex data/MGB3_EGY/txt/words_QATS.ctm data/MGB3_EGY/txt/$x.ctm words_$x.ctm
+    python makeFeat.py --wordindex words_$x.ctm data/MGB3_EGY/txt/{E,K,S}.txt.bw data/MGB3_EGY/txt/words_${x}_data.feat
 done 
-rm words_??.ctm
+rm words_*.ctm
 
 
 for x in data/GALE/txt/*bz2; do  bunzip2 $x;  done
